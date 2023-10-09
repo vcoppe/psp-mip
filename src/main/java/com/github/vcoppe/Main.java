33ef1dd8f4cb681e18be0ffe31de57c8d99d7b09
vcoppe <vianney.coppe@gmail.com>
2023-10-09 13:25:07 +0200

    same output as ddo

--- src/main/java/com/github/vcoppe/Main.java
+++ src/main/java/com/github/vcoppe/Main.java
@@ -63,8 +63,8 @@ public class Main {
 
         Locale.setDefault(Locale.US);
 
-        int objVal = (int) - Math.round(mip.objVal());
-        int bestBound = (int) - Math.round(mip.lowerBound());
+        int objVal = (int) Math.round(mip.objVal());
+        int bestBound = (int) Math.round(mip.lowerBound());
         System.out.printf("solver     : %s\n", "mip");
         System.out.printf("threads    : %d\n", threads);
         System.out.println("width      : 0");
@@ -76,8 +76,8 @@ public class Main {
         System.out.println("nb clusters: 0");
         System.out.printf("is exact   : %b\n", mip.hasProved());
         System.out.printf("duration   : %f\n", mip.runTime());
-        System.out.printf("best value : %d\n", - objVal);
-        System.out.printf("best bound : %d\n", - bestBound);
+        System.out.printf("best value : %d\n", objVal);
+        System.out.printf("best bound : %d\n", bestBound);
         System.out.println("expl. b&b  : 0");
         System.out.println("expl. dd   : 0");
         System.out.println("peak mem.  : 0");
